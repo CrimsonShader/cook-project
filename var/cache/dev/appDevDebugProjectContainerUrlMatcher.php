e66088e1fbc1a9cbf5e581b8cb20f42958fb2ae1
Samir SAIDI <s.saidi@fr.office.servicemagic.eu>
2018-07-17 16:00:15 +0200

    Lancement projet, premier bundle

--- var/cache/dev/appDevDebugProjectContainerUrlMatcher.php
+++ var/cache/dev/appDevDebugProjectContainerUrlMatcher.php
@@ -113,13 +113,9 @@ class appDevDebugProjectContainerUrlMatcher extends Symfony\Bundle\FrameworkBund
 
         }
 
-        // homepage
-        if ('' === $trimmedPathinfo) {
-            if (substr($pathinfo, -1) !== '/') {
-                return $this->redirect($pathinfo.'/', 'homepage');
-            }
-
-            return array (  '_controller' => 'AppBundle\\Controller\\DefaultController::indexAction',  '_route' => 'homepage',);
+        // hello_the_world
+        if ('/hello-world' === $pathinfo) {
+            return array (  '_controller' => 'OC\\PlatformBundle\\Controller\\AdvertController::indexAction',  '_route' => 'hello_the_world',);
         }
 
         throw 0 < count($allow) ? new MethodNotAllowedException(array_unique($allow)) : new ResourceNotFoundException();
